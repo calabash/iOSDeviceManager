ce1b85c84fc14b347070ad477cd64eacaefeac13
Joshua Moody <joshuajmoody@gmail.com>
2016-08-21 17:16:18 +0200

    Style: fix whitespace in PhysDevice and ShellRunner

--- iOSDeviceManager/Utilities/ShellRunner.m
+++ iOSDeviceManager/Utilities/ShellRunner.m
@@ -21,22 +21,22 @@
     NSTask *task = [[NSTask alloc] init];
     [task setLaunchPath: cmd];
     [task setArguments: args];
-    
+
     NSPipe *pipe = [NSPipe pipe];
     [task setStandardOutput: pipe];
-    
+
     NSFileHandle *file = [pipe fileHandleForReading];
-    
+
     [task launch];
-    
+
     [task waitUntilExit];
     if (task.terminationStatus != 0) {
         NSLog(@"Failed to execute command `%@` (Exit Status: %@)",  argString, @(task.terminationStatus));
         return nil;
     }
-    
+
     NSData *data = [file readDataToEndOfFile];
-    
+
     NSString *string = [[NSString alloc] initWithData: data encoding: NSUTF8StringEncoding];
     return [string componentsSeparatedByString:@"\n"];
 }
