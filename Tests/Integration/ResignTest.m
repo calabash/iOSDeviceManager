b3b3a0e61acb901d986ea0ba6d0b95c65bdf79c9
Joshua Moody <joshuajmoody@gmail.com>
2016-08-26 19:09:21 +0200

    BundleResigner: class for resigning bundles

---
+++ Tests/Integration/ResignTest.m
@@ -0,0 +1,61 @@
+
+#import "TestCase.h"
+#import "BundleResignerFactory.h"
+#import "BundleResigner.h"
+
+@interface ResignTest : TestCase
+
+@end
+
+@implementation ResignTest
+
+- (void)setUp {
+    [super setUp];
+}
+
+- (void)tearDown {
+    [super tearDown];
+}
+
+- (void)testResignWithSameIdentity {
+    NSString *identity = @"iPhone Developer: Karl Krukow (YTTN6Y2QS9)";
+    NSString *deviceUDID = defaultDeviceUDID;
+    NSString *bundlePath = tasky(ARM);
+
+    NSString *directory = [self.resources tmpDirectoryWithName:@"TaskyARM"];
+    NSString *target = [directory stringByAppendingPathComponent:[bundlePath lastPathComponent]];
+    [self.resources copyDirectoryWithSource:bundlePath
+                                     target:target];
+
+    bundlePath = target;
+
+    BundleResigner *resigner;
+    resigner = [[BundleResignerFactory shared]
+                                       resignerWithBundlePath:bundlePath
+                                                   deviceUDID:deviceUDID
+                                        signingIdentityString:identity];
+    expect([resigner resign]).to.equal(YES);
+}
+
+- (void)testResignWithDifferentIdentity {
+
+    NSString *identity = @"iPhone Developer: Joshua Moody (8QEQJFT59F)";
+    NSString *deviceUDID = defaultDeviceUDID;
+    NSString *bundlePath = runner(ARM);
+
+    NSString *directory = [self.resources tmpDirectoryWithName:@"RunnerARM"];
+    NSString *target = [directory stringByAppendingPathComponent:[bundlePath lastPathComponent]];
+    [self.resources copyDirectoryWithSource:bundlePath
+                                     target:target];
+
+    bundlePath = target;
+
+    BundleResigner *resigner;
+    resigner = [[BundleResignerFactory shared]
+                                       resignerWithBundlePath:bundlePath
+                                                   deviceUDID:deviceUDID
+                                        signingIdentityString:identity];
+    expect([resigner resign]).to.equal(YES);
+}
+
+@end
