90de8af971ff2635b8ddbefefcd2a86845452523
Joshua Moody <joshuajmoody@gmail.com>
2016-08-27 04:32:00 +0200

    AppUtils: more error logging when copying a bundle path

--- iOSDeviceManager/Utilities/AppUtils.m
+++ iOSDeviceManager/Utilities/AppUtils.m
@@ -1,5 +1,6 @@
 
 #import "AppUtils.h"
+#import "ShellRunner.h"
 
 @implementation AppUtils
 
@@ -11,40 +12,44 @@
 + (BOOL)appVersionIsDifferent:(NSDictionary *)oldPlist newPlist:(NSDictionary *)newPlist {
     NSString *oldShortVersionString = [self valueForKeyOrThrow:oldPlist key:@"CFBundleShortVersionString"];
     NSString *oldBundleVersion = [self valueForKeyOrThrow:oldPlist key:@"CFBundleVersion"];
-    
+
     NSString *newShortVersionString = [self valueForKeyOrThrow:newPlist key:@"CFBundleShortVersionString"];
     NSString *newBundleVersion = [self valueForKeyOrThrow:newPlist key:@"CFBundleVersion"];
-    
+
     if (![oldShortVersionString isEqualToString:newShortVersionString] ||
         ![oldBundleVersion isEqualToString:newBundleVersion]) {
         return YES;
     }
-    
+
     return NO;
 }
 
 + (NSString *)copyAppBundle:(NSString *)bundlePath {
-    NSError *e;
-    NSString *tempPath = [NSTemporaryDirectory() stringByAppendingPathComponent:
-                          [[NSProcessInfo processInfo] globallyUniqueString]];
-    [[NSFileManager defaultManager] createDirectoryAtPath:tempPath
-                              withIntermediateDirectories:YES
-                                               attributes:nil
-                                                    error:&e];
-    
-    if (e) {
-        NSLog(@"Error creating dir at path %@: %@", bundlePath, e);
+    NSError *error;
+    NSString *UUID = [[NSProcessInfo processInfo] globallyUniqueString];
+    NSString *tempPath = [NSTemporaryDirectory() stringByAppendingPathComponent:UUID];
+
+    if (![[NSFileManager defaultManager] createDirectoryAtPath:tempPath
+                                   withIntermediateDirectories:YES
+                                                    attributes:nil
+                                                         error:&error]) {
+        NSLog(@"ERROR: Could not create directory at path:\n    %@", tempPath);
+        NSLog(@"ERROR: while trying to copy an app bundle:\n    %@", bundlePath);
+        NSLog(@"ERROR: %@", [error localizedDescription]);
         return nil;
     }
 
     NSString *newBundlePath = [tempPath stringByAppendingPathComponent:bundlePath.lastPathComponent];
-    [[NSFileManager defaultManager] copyItemAtPath:bundlePath
-                                            toPath:newBundlePath
-                                             error:&e];
-    if (e) {
-        NSLog(@"Error copying bundle from %@ to %@: %@", bundlePath, tempPath, e);
+
+    if (![[NSFileManager defaultManager] copyItemAtPath:bundlePath
+                                                 toPath:newBundlePath
+                                                  error:&error]) {
+        NSLog(@"ERROR: Could not copy app bundle:\n    %@", bundlePath);
+        NSLog(@"ERROR: to tmp directory:\n    %@", newBundlePath);
+        NSLog(@"ERROR: %@", [error localizedDescription]);
         return nil;
     }
+
     return newBundlePath;
 }
 
