ca67176e9646fb06b31325688f0ad28c00086684
Joshua Moody <joshuajmoody@gmail.com>
2016-09-07 17:18:24 +0200

    Make: fbframeworks - use ditto
    * https://github.com/calabash/iOSDeviceManager/pull/44
    
    In addition to the symlink copy problem, these frameworks
    are codesigned which means ditto should be used to preserve
    signing attributes

--- bin/make/frameworks.sh
+++ bin/make/frameworks.sh
@@ -12,7 +12,18 @@ rm -rf ./Frameworks/*.framework
 HERE=$(pwd)
 
 (cd "${FBSIMCONTROL_PATH}";
- make frameworks;
- mkdir -p "${HERE}/Frameworks"
- cp -r build/Release/* "${HERE}/Frameworks")
+make frameworks;
+
+xcrun ditto build/Release/FBControlCore.framework \
+  "${HERE}/${OUTPUT_DIR}/Frameworks/FBControlCore.framework" ;
+
+xcrun ditto build/Release/FBDeviceControl.framework \
+  "${HERE}/${OUTPUT_DIR}/Frameworks/FBDeviceControl.framework" ;
+
+xcrun ditto build/Release/FBSimulatorControl.framework \
+  "${HERE}/${OUTPUT_DIR}/Frameworks/FBSimulatorControl.framework" ;
+
+xcrun ditto build/Release/XCTestBootstrap.framework \
+  "${HERE}/${OUTPUT_DIR}/Frameworks/XCTestBootstrap.framework" ;
+)
 
