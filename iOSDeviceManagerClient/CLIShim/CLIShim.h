230f938619811adcc390b8b4a0f89302fa08145f
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-09-29 08:14:12 -0700

    Added iOSDeviceManagerClient target

---
+++ iOSDeviceManagerClient/CLIShim/CLIShim.h
@@ -0,0 +1,6 @@
+
+#import <Foundation/Foundation.h>
+
+@interface CLIShim : NSObject
++ (void)process:(NSArray <NSString *> *)args;
+@end
---
+++ iOSDeviceManagerClient/CLIShim/CLIShim.m
@@ -0,0 +1,10 @@
+
+#import "CLIShim.h"
+
+@implementation CLIShim
++ (void)process:(NSArray <NSString *> *)args {
+    //TODO: Check if server is running.
+    //If not, start it.
+    //Then pass args to server. 
+}
+@end
