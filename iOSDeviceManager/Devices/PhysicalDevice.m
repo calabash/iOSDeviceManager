08f513b40303c8b4f3f622dd6fad575e23a8acc5
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-09-30 16:18:19 -0700

    Fix compilation error, Update fb frameworks

--- iOSDeviceManager/Devices/PhysicalDevice.m
+++ iOSDeviceManager/Devices/PhysicalDevice.m
@@ -4,6 +4,7 @@
 #import <FBDeviceControl/FBDeviceControl.h>
 #import <FBControlCore/FBControlCore.h>
 #import <XCTestBootstrap/XCTestBootstrap.h>
+#import "ServerConfig.h"
 #import "ShellRunner.h"
 #import "Codesigner.h"
 #import "AppUtils.h"
--- iOSDeviceManager/Server/ServerConfig.h
+++ iOSDeviceManager/Server/ServerConfig.h
@@ -11,5 +11,6 @@ typedef NS_ENUM(int, iOSReturnStatusCode) {
     iOSReturnStatusCodeInternalError,
     iOSReturnStatusCodeUnrecognizedCommand,
     iOSReturnStatusCodeUnrecognizedFlag,
-    iOSReturnStatusCodeDeviceNotFound
+    iOSReturnStatusCodeDeviceNotFound,
+    iOSReturnStatusCodeNoValidCodesignIdentity
 };
