31d1bda0cb861a1dc7c2e1a93ac27662a4b4a870
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-09-30 15:49:23 -0700

    Rm ObjectiveC client shim

--- iOSDeviceManagerClient/CLIShim/CLIShim.h
+++
@@ -1,7 +0,0 @@
-
-#import <Foundation/Foundation.h>
-#import "ServerConfig.h"
-
-@interface CLIShim : NSObject
-+ (iOSReturnStatusCode)process:(NSArray <NSString *> *)args;
-@end
--- iOSDeviceManagerClient/main.m
+++
@@ -1,8 +0,0 @@
-#import "CLIShim.h"
-
-int main(int argc, const char * argv[]) {
-    @autoreleasepool {
-        return [CLIShim process:[NSProcessInfo processInfo].arguments];
-    }
-    return 0;
-}
