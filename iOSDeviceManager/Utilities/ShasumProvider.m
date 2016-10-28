99fd5ac5eb323bd3177a62eb061002494ad4d92e
Austin Roos <austin@xamarin.com>
2016-10-28 11:14:28 +0200

    ShasumProvider: cast data.length to CC_LONG to suppress warning
    Signed-off-by: Joshua Moody <josmoo@microsoft.com>

--- iOSDeviceManager/Utilities/ShasumProvider.m
+++ iOSDeviceManager/Utilities/ShasumProvider.m
@@ -7,7 +7,7 @@
 + (NSString *)sha1FromData:(NSData *)data {
     uint8_t digest[CC_SHA1_DIGEST_LENGTH];
 
-    CC_SHA1(data.bytes, data.length, digest);
+    CC_SHA1(data.bytes, (CC_LONG)data.length, digest);
 
     NSMutableString *sha1 = [NSMutableString stringWithCapacity:CC_SHA1_DIGEST_LENGTH * 2];
 
