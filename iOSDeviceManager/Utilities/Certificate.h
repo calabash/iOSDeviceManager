9cd3f4c229ba091f30dd3498bc283d785fe29c7a
Joshua Moody <joshuajmoody@gmail.com>
2016-10-31 11:13:19 +0100

    Certificate: update todo in code comment

--- iOSDeviceManager/Utilities/Certificate.h
+++ iOSDeviceManager/Utilities/Certificate.h
@@ -1,12 +1,9 @@
 #import <Foundation/Foundation.h>
 
 // TODO: check to see if the certificate has expired or has been revoked
-// $ openssl x509 -enddate -inform der -in Tests/Resources/cert-from-CalabashWildcardProfile.cert
-// notAfter=Jan  6 10:41:35 2017 GMT
-// Unfortunately, the date could be in any of a number of formats.
 //
-// See the following SO post for tips on SecSecurity (OpenSSL).  The Apple OpenSSL docs
-// discourage the use of SecSecurity.
+// SO post has and an example of finding the expiration date.
+//
 // http://stackoverflow.com/questions/8850524/seccertificateref-how-to-get-the-certificate-information
 @interface Certificate : NSObject
 
