7510b711a75dc05d0394153c27d537b3de13c9ee
Joshua Moody <joshuajmoody@gmail.com>
2016-12-02 08:20:27 +0100

    Main: fixup after rebasing and resolving conflicts

--- iOSDeviceManager/main.m
+++ iOSDeviceManager/main.m
@@ -2,6 +2,7 @@
 #import "CLI.h"
 #import <FBControlCore/CalabashUtils.h>
 #import <CocoaLumberjack/CocoaLumberjack.h>
+#import "iOSDeviceManagerServer.h"
 
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
