5731cc4232d52b57323c8b7c0f6fd88e68786eaf
Christopher Fuentes <chris.fuentes@xamarin.com>
2016-12-16 13:54:23 -0800

    rm check for num required args vs num provided args

--- iOSDeviceManager/Commands/CLI.m
+++ iOSDeviceManager/Commands/CLI.m
@@ -108,14 +108,6 @@ static NSMutableDictionary <NSString *, Class> *commandClasses;
             if (ec != iOSReturnStatusCodeEverythingOkay) {
                 return ec;
             }
-            
-            //If no args present and none required, just print usage and exit.
-            NSInteger numRequiredArgs = [[command options] filteredArrayUsingPredicate:
-                                   [NSPredicate predicateWithFormat:@"SELF.required == YES"]].count;
-            if (cmdArgs.count == 0 && numRequiredArgs == 0) {
-                [command printUsage];
-                return iOSReturnStatusCodeEverythingOkay;
-            }
 
             //Ensure all required args are present
             for (CommandOption *opt in [command options]) {
