7ddfae36f137e736eee11864f18d166a6778302c
Joshua Moody <joshuajmoody@gmail.com>
2017-02-10 17:37:29 +0100

    Device: add FBCTestRunStrategy and FBiOSDeviceOperator categories

--- iOSDeviceManager/Devices/Device.h
+++ iOSDeviceManager/Devices/Device.h
@@ -3,10 +3,47 @@
 #import <FBSimulatorControl/FBSimulatorControl.h>
 #import <FBDeviceControl/FBDeviceControl.h>
 #import <Foundation/Foundation.h>
-#import "CodesignIdentity.h"
 #import <XCTestBootstrap/XCTestBootstrap.h>
 #import "Application.h"
 #import "iOSReturnStatusCode.h"
+#import "CodesignIdentity.h"
+
+
+@interface FBiOSDeviceOperator (iOSDeviceManagerAdditions)
+
+- (id<DVTApplication>)installedApplicationWithBundleIdentifier:(NSString *)bundleID;
+- (BOOL)isApplicationInstalledWithBundleID:(NSString *)bundleID error:(NSError **)error;
+- (BOOL)installApplicationWithPath:(NSString *)path error:(NSError **)error;
+- (BOOL)launchApplication:(FBApplicationLaunchConfiguration *)configuration error:(NSError **)error;
+
+@end
+
+@interface FBXCTestRunStrategy (iOSDeviceManagerAdditions)
+
+/**
+ Starts testing session with the assumption that the TestRunner is properly installed
+ and has an XCTestConfiguration file already
+
+
+ @param iOSTarget the simulator or device to target
+ @param bundleID TestRunner BundleID
+ @param sessionID testing session ID
+ @param attributes additional attributes used to start test runner
+ @param environment additional environment used to start test runner
+ @param reporter the Reporter to report test progress to.
+ @param logger the logger object to log events to, may be nil.
+ @param error If there is an error, upon return contains an NSError object that describes the problem.
+ @return testManager if the operation succeeds, otherwise nil.
+ */
++ (FBTestManager *)startTestManagerForIOSTarget:(id<FBiOSTarget>)iOSTarget
+                                 runnerBundleID:(NSString *)bundleID
+                                      sessionID:(NSUUID *)sessionID
+                                 withAttributes:(NSArray *)attributes
+                                    environment:(NSDictionary *)environment
+                                       reporter:(id<FBTestManagerTestReporter>)reporter
+                                         logger:(id<FBControlCoreLogger>)logger
+                                          error:(NSError *__autoreleasing *)error;
+@end
 
 @interface Device : NSObject
 
