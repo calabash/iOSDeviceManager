e1c296a79b12bfcf3ecb1d03c0cf4482bbae66a0
Joshua Escribano <escribirajoshua@gmail.com>
2017-02-23 12:43:04 -0800

    Adds commands to library project. Adds more commands to library.

--- iOSDeviceManager/LibraryWrapper/LibraryWrapper.m
+++ iOSDeviceManager/LibraryWrapper/LibraryWrapper.m
@@ -2,11 +2,46 @@
 
 #define STR( cString ) [NSString stringWithCString:( cString ) encoding:NSUTF8StringEncoding]
 
-int execute(char *command) {
+int launch_simulator(const char *simulatorID) {
     @autoreleasepool {
-        NSString *commandString = STR(command);
-        NSArray<NSString *> *args = [commandString componentsSeparatedByString:@" "];
-        
+        NSString *simID = STR(simulatorID);
+        NSArray<NSString *> *args = @[@"launch_simulator", simID];
+        return [CLI process:args];
+    }
+}
+
+int kill_simulator(const char *simulatorID) {
+    @autoreleasepool {
+        NSString *simID = STR(simulatorID);
+        NSArray<NSString *> *args = @[@"kill_simulator", simID];
+        return [CLI process:args];
+    }
+}
+
+int install_app(const char *pathToApp, const char *deviceID, const char *pathToProfile) {
+    @autoreleasepool {
+        NSString *appPath = STR(pathToApp);
+        NSString *deviceIDStr = STR(deviceID);
+        NSString *profilePath = STR(pathToProfile);
+        NSArray<NSString *> *args = @[@"install_app", appPath, deviceIDStr, profilePath];
+        return [CLI process:args];
+    }
+}
+
+int uninstall_app(const char *bundleID, const char *deviceID) {
+    @autoreleasepool {
+        NSString *deviceIDStr = STR(deviceID);
+        NSString *appID = STR(bundleID);
+        NSArray<NSString *> *args = @[@"uninstall_app", appID, deviceIDStr];
+        return [CLI process:args];
+    }
+}
+
+int is_installed(const char *bundleID, const char *deviceID) {
+    @autoreleasepool {
+        NSString *deviceIDStr = STR(deviceID);
+        NSString *appID = STR(bundleID);
+        NSArray<NSString *> *args = @[@"is_installed", appID, deviceIDStr];
         return [CLI process:args];
     }
 }
