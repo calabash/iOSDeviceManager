708ab78721677de15dd659076d424dc749af8724
Joshua Moody <joshuajmoody@gmail.com>
2017-03-03 08:18:17 +0100

    Resigning: update InstallAppCommand.m

--- iOSDeviceManager/Commands/InstallAppCommand.m
+++ iOSDeviceManager/Commands/InstallAppCommand.m
@@ -2,10 +2,12 @@
 #import <FBControlCore/FBControlCore.h>
 #import "ConsoleWriter.h"
 #import "AppUtils.h"
+#import "MobileProfile.h"
 
 static NSString *const APP_PATH_FLAG = @"-a";
 static NSString *const CODESIGN_IDENTITY_FLAG = @"-c";
 static NSString *const UPDATE_APP_FLAG = @"-u";
+static NSString *const PROFILE_PATH_FLAG = @"-p";
 
 @implementation InstallAppCommand
 + (NSString *)name {
@@ -17,21 +19,44 @@ static NSString *const UPDATE_APP_FLAG = @"-u";
     if ([[args allKeys] containsObject:UPDATE_APP_FLAG]) {
         update = [args[UPDATE_APP_FLAG] boolValue];
     }
-    
+
     Device *device = [self deviceFromArgs:args];
     if (!device) {
         return iOSReturnStatusCodeDeviceNotFound;
     }
-    
+
     NSString *pathToBundle= args[APP_PATH_FLAG];
     if ([args[APP_PATH_FLAG] hasSuffix:@".ipa"]) {
-        pathToBundle = [AppUtils unzipIpa:args[APP_PATH_FLAG]];
+        pathToBundle = [AppUtils unzipToTmpDir:args[APP_PATH_FLAG]];
+    } else {
+        pathToBundle = [AppUtils copyAppBundleToTmpDir:args[APP_PATH_FLAG]];
     }
-    
+
     Application *app = [Application withBundlePath:pathToBundle];
-    if (!app) {
+    if (!app || !app.path) {
+        ConsoleWriteErr(@"Error creating application object for path: %@", pathToBundle);
         return iOSReturnStatusCodeGenericFailure;
     }
+
+    CodesignIdentity *codesignIdentity = [self codesignIDFromArgs:args];
+
+    NSString *profilePath = args[PROFILE_PATH_FLAG];
+    MobileProfile *profile;
+    profile = [MobileProfile withPath:profilePath];
+
+    if (profile && codesignIdentity) {
+        ConsoleWriteErr(@"Mobile profile and codesign identity both specified - at most one needed");
+        return iOSReturnStatusCodeInvalidArguments;
+    }
+
+    if (profile) {
+        return [device installApp:app mobileProfile:profile shouldUpdate:update];
+    }
+
+    if (codesignIdentity) {
+        return [device installApp:app codesignIdentity:codesignIdentity shouldUpdate:update];
+    }
+
     return [device installApp:app shouldUpdate:update];
 }
 
@@ -52,18 +77,24 @@ static NSString *const UPDATE_APP_FLAG = @"-u";
                                                    info:@"Path .app bundle or .ipa"
                                                required:YES
                                              defaultVal:nil]];
-        [options addObject:[CommandOption withShortFlag:CODESIGN_IDENTITY_FLAG
-                                               longFlag:@"--codesign-identity"
-                                             optionName:@"codesign-identity"
-                                                   info:@"Identity used to codesign app bundle [device only]"
-                                               required:NO
-                                             defaultVal:@""]];
         [options addObject:[CommandOption withShortFlag:UPDATE_APP_FLAG
                                                longFlag:@"--update-app"
                                              optionName:@"true-or-false"
                                                    info:@"When true, will reinstall the app if the device contains an older version than the bundle specified"
                                                required:NO
                                              defaultVal:@(YES)]];
+        [options addObject:[CommandOption withShortFlag:PROFILE_PATH_FLAG
+                                               longFlag:@"--profile-path"
+                                             optionName:@"path/to/profile.mobileprovision"
+                                                   info:@"Path to provisioning profile"
+                                               required:NO
+                                             defaultVal:nil]];
+        [options addObject:[CommandOption withShortFlag:CODESIGN_IDENTITY_FLAG
+                                               longFlag:@"--codesign-identity"
+                                             optionName:@"codesign-identity"
+                                                   info:@"Identity used to codesign app bundle [device only]. Deprecated - should use profile path."
+                                               required:NO
+                                             defaultVal:@""]];
     });
     return options;
 }
