df3d2738c06eb4a12357a5e6133b3429913527a9
Joshua Moody <joshuajmoody@gmail.com>
2017-03-03 08:16:10 +0100

    Resigning: update Application

--- iOSDeviceManager/Application/Application.m
+++ iOSDeviceManager/Application/Application.m
@@ -9,41 +9,36 @@
 @implementation Application
 
 + (Application *)withBundlePath:(NSString *)pathToBundle {
-    Application *app = [Application new];
     NSFileManager *fileManager = [NSFileManager defaultManager];
-    
+
     if (!([fileManager fileExistsAtPath:pathToBundle] && [pathToBundle hasSuffix:@".app"])) {
         ConsoleWriteErr(@"Could not create application - path to bundle: %@ doesn't exist or not .app", pathToBundle);
         return nil;
     }
-    
-    app.path = pathToBundle;
-    
+
     NSString *plistPath = [pathToBundle stringByAppendingPathComponent:@"Info.plist"];
     if (![fileManager fileExistsAtPath:plistPath]) {
         ConsoleWriteErr(@"Could not find plist as path: %@", plistPath);
         return nil;
     }
-    
-    app.infoPlist = [NSDictionary dictionaryWithContentsOfFile:plistPath];
-    
-    NSString *executableName = app.infoPlist[@"CFBundleExecutable"];
+
+    NSDictionary *infoPlist = [NSDictionary dictionaryWithContentsOfFile:plistPath];
+
+    NSString *executableName = infoPlist[@"CFBundleExecutable"];
     NSString *executablePath = [pathToBundle stringByAppendingPathComponent:executableName];
     if (![fileManager fileExistsAtPath:executablePath]) {
         ConsoleWriteErr(@"Could not find bundle executable at path: %@", executablePath);
         return nil;
     }
-    
+
     NSError *archError;
     NSSet<NSString *> *arches = [FBBinaryParser architecturesForBinaryAtPath:executablePath error:&archError];
-    
+
     if (archError) {
         ConsoleWriteErr(@"Could not determine app architectures for executable at path: %@ \n with error: %@", executablePath, archError);
         return nil;
     }
-    
-    app.arches = arches;
-    
+
     NSError *productBundleErr;
     FBProductBundle *productBundle = [[[FBProductBundleBuilder builder]
                                       withBundlePath:pathToBundle]
@@ -52,9 +47,12 @@
         ConsoleWriteErr(@"Could not determine bundle id for bundle at: %@ \n withError: %@", pathToBundle, productBundleErr);
         return nil;
     }
-    
-    app.bundleID = productBundle.bundleID;
 
+
+    Application *app = [self withBundleID:productBundle.bundleID
+                                    plist:infoPlist
+                            architectures:arches];
+    app.path = pathToBundle;
     return app;
 }
 
@@ -63,8 +61,34 @@
     app.bundleID = bundleID;
     app.infoPlist = plist;
     app.arches = architectures;
-    
+
+    if ([architectures containsObject:@"x86_64"] ||
+        [architectures containsObject:@"i386"]) {
+        app.type = kApplicationTypeSimulator;
+    } else if ([architectures containsObject:@"arm"] ||
+               [architectures containsObject:@"armv7"] ||
+               [architectures containsObject:@"armv7s"] ||
+               [architectures containsObject:@"arm64"]) {
+        app.type = kApplicationTypePhysicalDevice;
+    } else {
+        app.type = kApplicationTypeUnknown;
+    }
+
     return app;
+}
+
+- (NSString *)baseDir {
+    NSString *dir = [self.path stringByDeletingLastPathComponent];
+    NSString *parentDirname = [dir lastPathComponent];
+    if ([parentDirname isEqualToString:@"Payload"]) {
+        //It's an unzipped ipa: /path/to/somewhere/Payload/MyApp.app
+        //We want to return '/path/to/somewhere'
+        return [dir stringByDeletingLastPathComponent];
+    } else {
+        //It's a simulator app or .app outside of a Payload context.
+        //We just return the parent dir.
+        return dir;
+    }
 }
 
 
