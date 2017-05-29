c22a8a5710c00e4dfcf11ae680a64aedf1a9a22a
Joshua Moody <joshuajmoody@gmail.com>
2017-05-29 14:41:18 +0200

    Bin: replace backtick with 'git checkout'
    Backticks are unnecessary

--- bin/xcode-build-phase/gitversioning-after.sh
+++ bin/xcode-build-phase/gitversioning-after.sh
@@ -5,8 +5,6 @@ echo "FATAL:  you must pass the path to IDMVersionDefines.h"
 exit 1
 fi
 
-VERSION_PATH="${1}"
-gitpath=`which git`
-
-echo "INFO: resetting content of ${VERSION_PATH}"
-`$gitpath checkout -- ${VERSION_PATH}`
+HEADER_FILE="${1}"
+echo "INFO: resetting content of ${HEADER_FILE}"
+git checkout -- "${HEADER_FILE}"
