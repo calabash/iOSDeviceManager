f35ba13e9837507eb06fc3d6f69f5e946877505e
Joshua Moody <joshuajmoody@gmail.com>
2017-08-15 08:03:47 +0200

    Test: remove unused imports in SimulatorTest

--- Tests/Integration/Devices/SimulatorTest.m
+++ Tests/Integration/Devices/SimulatorTest.m
@@ -4,8 +4,6 @@
 #import "Simulator.h"
 #import "ShellRunner.h"
 #import "ShellResult.h"
-#import "MachClock.h"
-#import <FBControlCore/FBControlCore.h>
 #import "Application.h"
 
 @interface Simulator (TEST)
