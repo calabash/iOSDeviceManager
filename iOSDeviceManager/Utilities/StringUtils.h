83efb6b7ff1663cd784b5ab3a3e9eea82f1e932d
Joshua Moody <joshuajmoody@gmail.com>
2017-08-15 08:40:28 +0200

    StringUtils: fix whitespace and indentation

--- iOSDeviceManager/Utilities/StringUtils.h
+++ iOSDeviceManager/Utilities/StringUtils.h
@@ -2,6 +2,7 @@
 #import <Foundation/Foundation.h>
 
 @interface NSString (CBXUtils)
+
 - (NSString *)replace:(NSString *)subs with:(NSString *)replacement;
 - (NSString *)subsFrom:(NSUInteger)start length:(NSUInteger)length;
 - (NSString *)plus:(NSString *)ending;
--- iOSDeviceManager/Utilities/StringUtils.m
+++ iOSDeviceManager/Utilities/StringUtils.m
@@ -2,6 +2,7 @@
 #import "StringUtils.h"
 
 @implementation NSString(CBXUtils)
+
 - (NSString *)replace:(NSString *)subs with:(NSString *)replacement {
     return [self stringByReplacingOccurrencesOfString:subs withString:replacement];
 }
@@ -9,6 +10,7 @@
 - (NSString *)subsFrom:(NSUInteger)start length:(NSUInteger)length {
     return [self substringWithRange:NSMakeRange(start, length)];
 }
+
 - (NSString *)plus:(NSString *)ending {
     return [self stringByAppendingString:ending];
 }
@@ -17,21 +19,24 @@
     return [self stringByAppendingPathComponent:pathComponent];
 }
 
-- (NSArray <NSString *> *)matching:(NSString *)regexStr options:(NSRegularExpressionOptions)options {
+- (NSArray <NSString *> *)matching:(NSString *)regexStr
+                           options:(NSRegularExpressionOptions)options {
     NSError *e;
     NSRegularExpression *regex = [NSRegularExpression regularExpressionWithPattern:regexStr
                                                                            options:options
                                                                              error:&e];
     NSAssert(e == nil, @"Error creating regex '%@': %@", regex, e);
-    
+
     NSMutableArray *matches = [NSMutableArray array];
     [regex enumerateMatchesInString:self
                             options:0
                               range:NSMakeRange(0, self.length)
-                         usingBlock:^(NSTextCheckingResult * _Nullable result, NSMatchingFlags flags, BOOL * _Nonnull stop) {
-        NSString *match = [self substringWithRange:[result range]];
-        [matches addObject:match];
-    }];
+                         usingBlock:^(NSTextCheckingResult * _Nullable result,
+                                      NSMatchingFlags flags,
+                                      BOOL * _Nonnull stop) {
+                             NSString *match = [self substringWithRange:[result range]];
+                             [matches addObject:match];
+                         }];
     return matches;
 }
 
