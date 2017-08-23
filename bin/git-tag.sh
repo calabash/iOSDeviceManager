f201359a7c3bca2505eed8ba76b24723b206ed29
Joshua Moody <joshuajmoody@gmail.com>
2017-08-23 15:34:01 +0200

    Bin: add script for git tagging releases

---
+++ bin/git-tag.sh
@@ -0,0 +1,22 @@
+#!/usr/bin/env bash
+
+set -e
+
+source bin/log.sh
+
+BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD \
+  | tr -d "\n")
+
+if [ "${BRANCH}" != "master" ]; then
+  error "Only create tags from the master branch"
+  error "The current branch is: ${BRANCH}"
+  exit 1
+fi
+
+TAG=$(cat iOSDeviceManager/Commands/VersionCommand.m \
+  | grep "VERSION = @" \
+  | perl -pe '($_)=/([0-9]+([.][0-9]+)+)/')
+
+git tag -a "${TAG}" -m"${TAG}"
+git push origin "${TAG}"
+git branch "tag/${TAG}" "${TAG}"
