c4ddccd0381f50d90ba153c327f9dad5d40c14f9
Joshua Moody <joshuajmoody@gmail.com>
2017-08-23 15:30:16 +0200

    Make: update scripts to use common simctl loading function

--- bin/make/build.sh
+++ bin/make/build.sh
@@ -1,14 +1,11 @@
 #!/usr/bin/env bash
 
 set +e
-
-# Force Xcode 8 CoreSimulator env to be loaded so xcodebuild does not fail.
-for try in {1..4}; do
-  xcrun simctl help &>/dev/null
-  sleep 1.0
-done
-
+source bin/simctl.sh
+ensure_valid_core_sim_service
 set -e
+
+source bin/log.sh
 
 BUILD_DIR="build"
 XC_PROJECT="iOSDeviceManager.xcodeproj"
--- bin/make/test-integration.sh
+++ bin/make/test-integration.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set +e
+source bin/simctl.sh
+ensure_valid_core_sim_service
 set -e
 
 source bin/log.sh
