f1cf8e5fe0fc6c1a4f62cf2ed4def908745cd8ca
Joshua Moody <joshuajmoody@gmail.com>
2017-09-13 00:48:31 +0200

    Bump version to 2.1.0

--- iOSDeviceManager/Commands/VersionCommand.m
+++ iOSDeviceManager/Commands/VersionCommand.m
@@ -3,7 +3,7 @@
 #import "IDMVersionDefines.h"
 #import "JSONUtils.h"
 
-static NSString *const VERSION = @"2.0.2";
+static NSString *const VERSION = @"2.1.0";
 static NSString *const JSON_VERSION_FLAG = @"-j";
 static NSString *const JSON_VERSION_OPTION_NAME = @"json";
 
