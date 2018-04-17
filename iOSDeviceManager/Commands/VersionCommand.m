ae589e0fe2c6ae46911cf6ba991de9d616db08c4
Joshua Moody <joshuajmoody@gmail.com>
2018-04-17 08:40:05 +0200

    Bin: update the version to 3.1.0

--- iOSDeviceManager/Commands/VersionCommand.m
+++ iOSDeviceManager/Commands/VersionCommand.m
@@ -4,7 +4,7 @@
 #import "IDMVersionDefines.h"
 #import "JSONUtils.h"
 
-static NSString *const VERSION = @"3.0.0";
+static NSString *const VERSION = @"3.1.0";
 static NSString *const JSON_VERSION_FLAG = @"-j";
 static NSString *const JSON_VERSION_OPTION_NAME = @"json";
 
