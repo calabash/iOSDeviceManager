4d2137ab5d8a7f07fe553fc5a8c5652030adc8f1
Joshua Moody <joshuajmoody@gmail.com>
2018-05-08 08:17:10 +0200

    Spec: run tests against connected physical devices

--- spec/is_installed_spec.rb
+++ spec/is_installed_spec.rb
@@ -26,30 +26,46 @@ describe "is-installed" do
   end
 
   context "physical devices" do
-    let(:device) { IDM::Resources.instance.default_physical_device}
-    let(:udid) { device.udid }
+    IDM::Resources.instance.xcode_install_paths.each do |developer_dir|
+      IDM::Resources.instance.with_developer_dir(developer_dir) do
+        # Add a simulator to this list of of devices
+        devices = IDM::Resources.instance.physical_devices
+        xcode_version = developer_dir[/(\d+\.\d+(\.\d+)?)/]
+        if devices.empty?
+          it "Xcode #{xcode_version} no compatible devices connected via USB" do
+            expect(true).to be_truthy
+          end
+        else
+          context "#{developer_dir}" do
+            let(:app) { RunLoop::App.new(IDM::Resources.instance.test_app(:arm)) }
+            # For every connected (compatible) device
+            devices.each do |device|
+              context "#{device.name} (#{device.version.to_s})" do
+                # Run these tests
+                let(:udid) { device.udid }
 
-    it "prints true if app is installed" do
-      if device && device != ""
-        args = ["is-installed", "com.apple.Preferences", "--device-id", udid]
-        hash = IDM.shell(args)
-        expect(hash[:out].split($-0).last).to be == "true"
-        expect(hash[:exit_status]).to be == IDM.exit_status(:success)
-      else
-        pending("No physical device is connected")
-        fail
-      end
-    end
+                it "prints true if app is installed" do
+                  if device && device != ""
+                    args = ["is-installed", "com.apple.Preferences", "--device-id", udid]
+                    hash = IDM.shell(args)
+                    expect(hash[:out].split($-0).last).to be == "true"
+                    expect(hash[:exit_status]).to be == IDM.exit_status(:success)
+                  else
+                    pending("No physical device is connected")
+                    fail
+                  end
+                end
 
-    it "prints false if app is not installed" do
-      if device && device != ""
-        args = ["is-installed", "com.apple.NoSuchApp", "--device-id", udid]
-        hash = IDM.shell(args)
-        expect(hash[:out].split($-0).last).to be == "false"
-        expect(hash[:exit_status]).to be == IDM.exit_status(:false)
-      else
-        pending("No physical device is connected")
-        fail
+                it "prints false if app is not installed" do
+                  args = ["is-installed", "com.apple.NoSuchApp", "--device-id", udid]
+                  hash = IDM.shell(args)
+                  expect(hash[:out].split($-0).last).to be == "false"
+                  expect(hash[:exit_status]).to be == IDM.exit_status(:false)
+                end
+              end
+            end
+          end
+        end
       end
     end
   end
--- spec/upload-xcappdata_spec.rb
+++ spec/upload-xcappdata_spec.rb
@@ -1,39 +1,56 @@
 
-if IDM::Resources.instance.physical_device_connected?
-  describe "upload-xcappdata" do
+describe "upload-xcappdata" do
+  IDM::Resources.instance.xcode_install_paths.each do |developer_dir|
+    IDM::Resources.instance.with_developer_dir(developer_dir) do
+      # Add a simulator to this list of of devices
+      devices = IDM::Resources.instance.physical_devices
+      xcode_version = developer_dir[/(\d+\.\d+(\.\d+)?)/]
+      if devices.empty?
+        it "Xcode #{xcode_version} no compatible devices connected via USB" do
+          expect(true).to be_truthy
+        end
+      else
+        context "#{developer_dir}" do
+          let(:app) { IDM::Resources.instance.test_app(:arm) }
+          # For every connected (compatible) device
+          devices.each do |device|
+            context "#{device.name} (#{device.version.to_s})" do
+              # Run these tests
+              let(:xcappdata) do
+                appdata = File.join(IDM::Resources.instance.tmp_dir("xcappdata"),
+                                    "New.xcappdata")
 
-    let(:device) { IDM::Resources.instance.physical_device }
-    let(:app) { IDM::Resources.instance.test_app(:arm) }
-    let(:xcappdata) do
-      appdata = File.join(IDM::Resources.instance.tmp_dir("xcappdata"),
-                          "New.xcappdata")
+                args = ["generate-xcappdata", appdata, "--overwrite",  "1"]
+                hash = IDM.shell(args)
+                hash[:out]
 
-      args = ["generate-xcappdata", appdata]
-      hash = IDM.shell(args)
-      hash[:out]
+                documents = File.join(appdata, "AppData", "Documents")
+                #FileUtils.mkdir_p(documents)
 
-      documents = File.join(appdata, "AppData", "Documents")
-      FileUtils.mkdir_p(documents)
+                path = File.join(documents,
+                                 "#{Time.now.strftime("%Y-%m-%d-%H-%M-%S")}.txt")
+                File.open(path, "w") do |file|
+                  file.puts("content")
+                end
 
-      path = File.join(documents,
-                       "#{Time.now.strftime("%Y-%m-%d-%H-%M-%S")}.txt")
-      File.open(path, "w") do |file|
-        file.puts("content")
+                hash[:out]
+              end
+
+              it "uploads xcappdata" do
+                args = ["install", app, "--device-id", device.udid]
+                hash = IDM.shell(args)
+                expect(hash[:exit_status]).to be == IDM.exit_status(:success)
+
+                args = ["upload-xcappdata", app, xcappdata,
+                        "--device-id", device.udid]
+                hash = IDM.shell(args)
+
+                expect(hash[:exit_status]).to be == IDM.exit_status(:success)
+              end
+            end
+          end
+        end
       end
-
-      hash[:out]
-    end
-
-    it "uploads xcappdata" do
-      args = ["install", app, "--device-id", device.udid]
-      hash = IDM.shell(args)
-      expect(hash[:exit_status]).to be == IDM.exit_status(:success)
-
-      args = ["upload-xcappdata", app, xcappdata, "--device-id", device.udid]
-      hash = IDM.shell(args)
-
-      expect(hash[:exit_status]).to be == IDM.exit_status(:success)
-      expect(hash[:out]).to be == ""
     end
   end
 end
