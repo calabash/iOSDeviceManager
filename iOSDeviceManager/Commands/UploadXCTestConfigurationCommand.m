ba4c82bd545811799be826811e850049add7be82
Joshua Moody <josmoo@microsoft.com>
2018-08-31 10:20:44 +0200

    xctestconf: upload command requires local file paths

--- iOSDeviceManager/Commands/UploadXCTestConfigurationCommand.m
+++ iOSDeviceManager/Commands/UploadXCTestConfigurationCommand.m
@@ -11,20 +11,6 @@
     return @"upload-xctestconf";
 }
 
-+ (BOOL) prepareBundle:(NSString *)appPath forDevice:(Device *)device {
-    if (![device isInstalled:appPath withError:nil]) {
-        if ([appPath isUniformTypeIdentifier]) {
-            ConsoleWriteErr(@"Application %@ is not installed on device %@."
-                            "Provide path to a .app bundle or .ipa archive instead",
-                            appPath, [device uuid]);
-            return NO;
-        }
-        [device installApp:[Application withBundlePath:[FileUtils expandPath:appPath]]
-            forceReinstall:NO];
-    }
-    return YES;
-}
-
 + (iOSReturnStatusCode)execute:(NSDictionary *)args {
     Device *device = [self deviceFromArgs:args];
     if (!device) {
@@ -33,44 +19,58 @@
 
     NSString *AUTbundleIdOrPath = args[BUNDLE_ID_OPTION_NAME];
     NSString *runnerBundleIdOrPath = args[RUNNER_BUNDLE_ID_OPTION_NAME];
-    NSString *AUTBundleId, *runnerBundleId;
-    if (![AUTbundleIdOrPath isUniformTypeIdentifier]) {
+    NSString *runnerPath, *AUTPath = nil;
+
+    if ([AUTbundleIdOrPath isUniformTypeIdentifier]) {
+        ConsoleWriteErr(@"Invalid argument: '%@'\n"
+                        "Expected a path to an .app bundle.", AUTbundleIdOrPath);
+        return iOSReturnStatusCodeInvalidArguments;
+    } else {
         NSString *expanded = [FileUtils expandPath:AUTbundleIdOrPath];
         BOOL exists = [Application appBundleOrIpaArchiveExistsAtPath:expanded];
         if (!exists) {
-            ConsoleWriteErr(@"The argument:\n  %@\n is not a valid bundle identifier, "
-                            "path to a .app bundle, or a .ipa archive", AUTbundleIdOrPath);
+            ConsoleWriteErr(@"AUT .app does not exist at path:\n",
+                            AUTbundleIdOrPath);
             return iOSReturnStatusCodeInvalidArguments;
         }
-
-        AUTBundleId = [[Application withBundlePath:expanded] bundleID];
-    } else {
-        AUTBundleId = [AUTbundleIdOrPath copy];
+        AUTPath = expanded;
     }
 
-    if (![runnerBundleIdOrPath isUniformTypeIdentifier]) {
+    if ([runnerBundleIdOrPath isUniformTypeIdentifier]) {
+        ConsoleWriteErr(@"Invalid argument: '%@'\n"
+                        "Expected a path to a -Runner.app bundle.",
+                        runnerBundleIdOrPath);
+        return iOSReturnStatusCodeInvalidArguments;
+    } else {
         NSString *expanded = [FileUtils expandPath:runnerBundleIdOrPath];
         BOOL exists = [Application appBundleOrIpaArchiveExistsAtPath:expanded];
         if (!exists) {
-            ConsoleWriteErr(@"The argument:\n  %@\n is not a valid bundle identifier, "
-                            "path to a .app bundle, or a .ipa archive", AUTbundleIdOrPath);
+            ConsoleWriteErr(@"-Runner.app does not exist at path:\n",
+                            runnerBundleIdOrPath);
             return iOSReturnStatusCodeInvalidArguments;
         }
-
-        runnerBundleId = [[Application withBundlePath:expanded] bundleID];
-    } else {
-        runnerBundleId = [runnerBundleIdOrPath copy];
+        runnerPath = expanded;
     }
 
-    if (![self prepareBundle:AUTbundleIdOrPath forDevice:device] ||
-        ![self prepareBundle:runnerBundleIdOrPath forDevice:device]) {
+
+    iOSReturnStatusCode code;
+    code = [device installApp:[Application withBundlePath:AUTPath]
+               forceReinstall:YES];
+    if (code != iOSReturnStatusCodeEverythingOkay) {
+        return iOSReturnStatusCodeInternalError;
+    }
+
+    code = [device installApp:[Application withBundlePath:runnerPath]
+               forceReinstall:YES];
+    if (code != iOSReturnStatusCodeEverythingOkay) {
         return iOSReturnStatusCodeInternalError;
     }
 
     NSError *error = nil;
-    if ([device stageXctestConfigurationToTmpForRunnerBundleIdentifier:runnerBundleId
-                                                   AUTBundleIdentifier:AUTBundleId
-                                                                 error:&error]) {
+    if ([device stageXctestConfigurationToTmpForRunner:runnerPath
+                                                   AUT:AUTPath
+                                            deviceUDID:device.uuid
+                                                 error:&error]) {
         return iOSReturnStatusCodeEverythingOkay;
     } else {
         ConsoleWriteErr(@"%@", error);
@@ -86,12 +86,12 @@
           @[
               [CommandOption withPosition:0
                                optionName:BUNDLE_ID_OPTION_NAME
-                                     info:@"bundle identifier or path to .app on disk"
+                                     info:@"path to application under test .app on disk"
                                  required:YES
                                defaultVal:nil],
               [CommandOption withPosition:1
                                optionName:RUNNER_BUNDLE_ID_OPTION_NAME
-                                     info:@"runner bundle identifier or path to .app on disk"
+                                     info:@"path to test -Runner.app on disk"
                                  required:YES
                                defaultVal:nil],
               [CommandOption withShortFlag:DEVICE_ID_FLAG
