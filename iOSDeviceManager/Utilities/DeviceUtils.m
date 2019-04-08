e3f28f4b737138246c041a556a323117e5ae0b4f
Maxim Lobanov <v-malob@microsoft.com>
2019-04-08 15:56:47 +0300

    adapt iOSDeviceManager for new framework

--- iOSDeviceManager/Utilities/DeviceUtils.m
+++ iOSDeviceManager/Utilities/DeviceUtils.m
@@ -139,12 +139,19 @@ const double EPSILON = 0.001;
 + (NSString *)defaultSimulator {
     int major = XcodeUtils.versionMajor + 2;
     int minor = XcodeUtils.versionMinor;
-
+    
     if (XcodeUtils.versionMajor == 10) {
-        return [NSString
-                stringWithFormat:@"iPhone XS (%d.%d)",
-                major,
-                minor];
+        if (XcodeUtils.versionMinor < 2) {
+            return [NSString
+                    stringWithFormat:@"iPhone XS (%d.%d)",
+                    major,
+                    minor];
+        } else {
+            return [NSString
+                    stringWithFormat:@"iPhone Xs (%d.%d)",
+                    major,
+                    minor];
+        }
     } else {
         return [NSString
                 stringWithFormat:@"iPhone %d (%d.%d)",
