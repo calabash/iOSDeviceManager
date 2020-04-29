002fdad9e9e35306f062e12ad68018f7f5e393e6
Joshua Moody <josmoo@microsoft.com>
2020-04-29 21:30:44 +0200

    ppp: implement the entitlement compare function

--- ppp/lib/entitlement.js
+++ ppp/lib/entitlement.js
@@ -9,13 +9,17 @@ global.EntitlementComparisonResult = {
     // Accept
     ProfileHasKeyExactly: 1,
     ProfileHasKey: 100,
-    ProfileHasUnrequiredKey: 1000
+    ProfileHasUnRequiredKey: 1000
 };
 
 class Entitlement {
   constructor(key, value) {
     this.key = key;
-    this.value = value;
+    if (value === undefined) {
+      this.value = null;
+    } else {
+      this.value = value;
+    }
   }
 
   hasArrayValue() {
@@ -30,9 +34,129 @@ class Entitlement {
     return (typeof this.value === "boolean");
   }
 
-  static compare(profileEntitlement, appEntitlement) {
-    return EntitlementComparisonResult.AppNorProfileHasKey;
+  static compareEntitlementArrays(appArray, profileArray) {
+    if (appArray.length > profileArray.length) {
+      return this.compareLargerAppArray(appArray, profileArray);
+    } else if (appArray.length < profileArray.length) {
+      return this.compareLargerProfileArray(appArray, profileArray);
+    } else {
+      return this.compareEqualSizedArrays(appArray, profileArray);
+    }
   }
+
+  static compareEqualSizedArrays(appArray, profileArray) {
+    // We know they are equal sized
+    for (let elm of appArray) {
+      if (!profileArray.includes(elm)) {
+        return EntitlementComparisonResult.ProfileHasKey;
+      }
+    }
+
+    for (let elm of profileArray) {
+      if (!appArray.includes(elm)) {
+        return EntitlementComparisonResult.ProfileHasKey;
+      }
+    }
+    return EntitlementComparisonResult.ProfileHasKeyExactly;
+  }
+
+  static compareLargerProfileArray(appArray, profileArray) {
+    // Prefer profiles with _fewer_ entitlements
+    return (profileArray.length - appArray.length) *
+      EntitlementComparisonResult.ProfileHasKey;
+  }
+
+  static compareLargerAppArray(appArray, profileArray) {
+    // Can match if profile has one element that is the wildcard or wildcard
+    // Team ID.
+    //
+    // Maybe this could be expanded to 'profile has _any_ element that is
+    // the wildcard or wildcard Team ID
+    if (profileArray.length === 1) {
+      const profileValue = profileArray[0];
+      if (profileValue === "*" ||
+        // e.g. FYD86LA7RE.* - looks like a Team ID
+        (profileValue.length === 12 && profileValue.endsWith("*"))) {
+        return EntitlementComparisonResult.ProfileHasKey;
+      }
+    }
+    return EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+  }
+
+  static compare(profileEntitlement, appEntitlement) {
+    if (appEntitlement.value && (profileEntitlement.value === null)) {
+      return EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+    }
+
+    if (appEntitlement.value === null) {
+      if (profileEntitlement.value === null) {
+        return EntitlementComparisonResult.AppNorProfileHasKey;
+      } else {
+        return EntitlementComparisonResult.ProfileHasUnRequiredKey;
+      }
+    }
+
+    if (appEntitlement.hasArrayValue()) {
+      if (profileEntitlement.hasArrayValue()) {
+        const appArray = appEntitlement.value;
+        const profileArray = profileEntitlement.value;
+        return this.compareEntitlementArrays(appArray, profileArray);
+      } else if (profileEntitlement.hasStringValue()) {
+        if (profileEntitlement.value === "*") {
+          return EntitlementComparisonResult.ProfileHasKey;
+        } else {
+          return EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+        }
+      } else {
+        // Profile entitlement is neither a string or array.
+        // Assume profile does not have the required key.
+      }
+    } else if (appEntitlement.hasStringValue()) {
+      if (profileEntitlement.hasStringValue()) {
+        if (appEntitlement.value === profileEntitlement.value) {
+          return EntitlementComparisonResult.ProfileHasKeyExactly;
+        } else {
+          return EntitlementComparisonResult.ProfileHasKey;
+        }
+      } else if (profileEntitlement.hasArrayValue()) {
+         if (appEntitlement.value === "*") {
+           // I don't understand this one.
+           return EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+         } else {
+           const profileArr = profileEntitlement.value;
+           if (profileArr.includes(appEntitlement.value)) {
+             return EntitlementComparisonResult.ProfileHasKey;
+           } else {
+             return EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+           }
+         }
+      } else {
+        // Profile entitlment is neither a string or array.
+        // Assume profile does not have the required key.
+      }
+    } else if (appEntitlement.hasBoolValue()) {
+      if (profileEntitlement.hasBoolValue()) {
+        console.log("in the bool / bool case");
+        if (appEntitlement.value == profileEntitlement.value) {
+          return EntitlementComparisonResult.ProfileHasKeyExactly;
+        } else {
+          // Maybe revisit this
+          return EntitlementComparisonResult.ProfileHasKey;
+        }
+      } else {
+         return EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+      }
+    }
+
+    console.error("Unable to match entitlement, unexpected type(s)");
+    console.error(`                      key: ${profileEntitlement.key}`);
+    console.error(` profile entitlement type: ${typeof(profileEntitlement.value)}`);
+    console.error(`profile entitlement value: ${profileEntitlement.value}`);
+    console.error(`     app entitlement type: ${typeof(appEntitlement.value)}`);
+    console.error(`    app entitlement value: ${appEntitlement.value}`);
+    return EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+  }
+
 }
 
 module.exports = Entitlement;
--- ppp/test/lib/entitlement.test.js
+++ ppp/test/lib/entitlement.test.js
@@ -8,10 +8,33 @@ describe("EntitlementComparisonResult", () => {
 });
 
 describe("Entitlement", () => {
-  it("is a class with a constructor", () => {
-    const instance = new Entitlement("key", "value");
-    expect(instance.key).to.equal("key");
-    expect(instance.value).to.equal("value");
+
+  describe(".contructor", () => {
+    it("binds key and value correctly", () => {
+      const instance = new Entitlement("key", "value");
+      expect(instance.key).to.equal("key");
+      expect(instance.value).to.equal("value");
+    });
+
+    it("handles null and undefined values correctly", () => {
+      let instance = new Entitlement("key", undefined);
+      expect(instance.value).to.equal(null);
+
+      instance = new Entitlement("key", null);
+      expect(instance.value).to.equal(null);
+    });
+
+    it("handles true/false liternals", () => {
+      let a = new Entitlement("key", false);
+      let b = new Entitlement("key", false);
+
+      expect(a.value === b.value).to.equal(true);
+
+      a = new Entitlement("key", true);
+      b = new Entitlement("key", true);
+
+      expect(a.value === b.value).to.equal(true);
+    });
   });
 
   describe("#hasArrayValue", () => {
@@ -57,8 +80,245 @@ describe("Entitlement", () => {
   });
 
   describe(".compare", () => {
-    it("returns profile does not have required key", () => {
-      expect(Entitlement.compare(null, null)).to.equal(0);
+    describe("simple cases", () => {
+      it("returns ProfileDoesNotHaveRequiredKey when app has entitlement " +
+        "but profile does not", () => {
+          const app = new Entitlement("key", ["a", "b", "c"]);
+          const profile = new Entitlement("key", null);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+          expect(actual).to.equal(expected);
+        });
+
+      it("returns AppNorProfileHasKey when app does not contain the entitlement " +
+        "and the profile does not contain the entitlement", () => {
+          const app = new Entitlement("key", null);
+          const profile = new Entitlement("key", null);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.AppNorProfileHasKey;
+          expect(actual).to.equal(expected);
+        });
+
+      it("returns ProfileHasUnRequiredKey when app does not have the entitlement " +
+        "but the profile does have the entitlement", () => {
+          const app = new Entitlement("key", null);
+          const profile = new Entitlement("key", "value");
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileHasUnRequiredKey;
+          expect(actual).to.equal(expected);
+        });
+    });
+
+    describe("app entitlement is an array", () => {
+      let app = new Entitlement("key", ["a", "b", "c"]);
+
+      describe("profile entitlement is an array", () => {
+        describe("app array count is > profile array count", () => {
+          describe("profile array count == 1", () => {
+            it("returns ProfileHasKey when profile[0] is == '*'", () => {
+              const profile = new Entitlement("key", ["*"]);
+
+              actual = Entitlement.compare(profile, app);
+              expected = EntitlementComparisonResult.ProfileHasKey;
+              expect(actual).to.equal(expected);
+            });
+
+            // suspicious!
+            it("returns ProfileHasKey when profile[0] looks like a Team ID", () => {
+              const profile = new Entitlement("key", ["FYD86LA7RE.*"]);
+
+              actual = Entitlement.compare(profile, app);
+              expected = EntitlementComparisonResult.ProfileHasKey;
+              expect(actual).to.equal(expected);
+            });
+
+            it("returns ProfileDoesNotHaveRequiredKey in other cases", () => {
+              const profile = new Entitlement("key", ["ABCE"]);
+
+              actual = Entitlement.compare(profile, app);
+              expected = EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+              expect(actual).to.equal(expected);
+            });
+          });
+
+          describe("profile array count != 1", () => {
+            it("return ProfileDoesNotHaveRequiredKey", () => {
+              const profile = new Entitlement("key", ["a", "b"]);
+
+              actual = Entitlement.compare(profile, app);
+              expected = EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+              expect(actual).to.equal(expected);
+            });
+          });
+        });
+
+        describe("app array count < profile array count", () => {
+          it("returns (profile count - app count) * ProfileHasKey", () => {
+            const profile = new Entitlement("key", ["a", "b", "c", "d", "e"]);
+
+            actual = Entitlement.compare(profile, app);
+            expected = EntitlementComparisonResult.ProfileHasKey * 2
+            expect(actual).to.equal(expected);
+          });
+        });
+
+        describe("app array count == profile count", () => {
+          it("returns ProfileHasKeyExactly when app and profile arrays are " +
+            "the same", () => {
+              const profile = new Entitlement("key", ["a", "b", "c"]);
+
+              actual = Entitlement.compare(profile, app);
+              expected = EntitlementComparisonResult.ProfileHasKeyExactly;
+              expect(actual).to.equal(expected);
+            });
+          it("returns ProfileHasKey otherwise", () => {
+              const profile = new Entitlement("key", ["a", "b", "d"]);
+
+              actual = Entitlement.compare(profile, app);
+              expected = EntitlementComparisonResult.ProfileHasKey;
+              expect(actual).to.equal(expected);
+          });
+        });
+      });
+
+      describe("profile entitlement is a string", () => {
+        it("returns ProfileHasKey if string value is '*'", () => {
+          const profile = new Entitlement("key", "*");
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileHasKey;
+          expect(actual).to.equal(expected);
+        });
+
+        it("returns ProfileDoesNotHaveRequiredKey otherwise", () => {
+          const profile = new Entitlement("key", "not a wildcard");
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+          expect(actual).to.equal(expected);
+        });
+      });
+
+      it("returns ProfileDoesNotHaveRequiredKey when profile value is not a " +
+        "string or array", () => {
+          const profile = new Entitlement("key", 6.23);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+          expect(actual).to.equal(expected);
+       });
+    });
+
+    describe("app entitlement is string", () => {
+      let app = new Entitlement("key", "string entitlement");
+
+      describe("profile entitlement is a string", () => {
+        it("returns ProfileHasKeyExactly when app and profile are exactly the " +
+          "same", () => {
+            const profile = new Entitlement("key", app.value);
+
+            actual = Entitlement.compare(profile, app);
+            expected = EntitlementComparisonResult.ProfileHasKeyExactly;
+            expect(actual).to.equal(expected);
+        });
+
+        it("returns ProfileHasKey otherwise", () => {
+            const profile = new Entitlement("key", "string value");
+
+            actual = Entitlement.compare(profile, app);
+            expected = EntitlementComparisonResult.ProfileHasKey;
+            expect(actual).to.equal(expected);
+        });
+      });
+
+      describe("profile entitlement is an array", () => {
+        it("returns ProfileDoesNotHaveRequiredKey when app entitlement is '*'", () => {
+          app = new Entitlement("key", "*");
+          const profile = new Entitlement("key", ["a", "b", "c"]);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+          expect(actual).to.equal(expected);
+        });
+
+        it("returns ProfileHasKey if profile array contains app value", () => {
+          // let does not work like rspec let.  if 'app' is assigned here
+          // it's value persists across tests. :(
+          const otherApp = new Entitlement("key", "a");
+          const profile = new Entitlement("key", ["a", "b", "c"]);
+
+          actual = Entitlement.compare(profile, otherApp);
+          expected = EntitlementComparisonResult.ProfileHasKey;
+          expect(actual).to.equal(expected);
+        });
+
+        it("returns ProfileDoesNotHaveRequiredKey when profile does not " +
+          "contain app value", () => {
+          const profile = new Entitlement("key", ["a", "b", "c"]);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+          expect(actual).to.equal(expected);
+        });
+      });
+
+      it("returns ProfileDoesNotHaveRequiredKey when profile entitlement is " +
+        "not a string or array", () => {
+          const profile = new Entitlement("key", 11.11);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+          expect(actual).to.equal(expected);
+      });
+    });
+
+    describe("app entitlement is a bool", () => {
+      describe("profile entitlement is a bool", () => {
+        it("returns ProfileDoesNotHaveRequiredKey when profile entitlement " +
+          "is not a bool value", () => {
+          const app = new Entitlement("key", false);
+          const profile = new Entitlement("key", "string");
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileDoesNotHaveRequiredKey;
+          expect(actual).to.equal(expected);
+        });
+
+        it("returns ProfileHasKeyExactly if app value == profile value", () => {
+          let app = new Entitlement("key", true);
+          let profile = new Entitlement("key", true);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileHasKeyExactly;
+          expect(actual).to.equal(expected);
+
+          app = new Entitlement("key", true);
+          profile = new Entitlement("key", true);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileHasKeyExactly;
+          expect(actual).to.equal(expected);
+        });
+
+        it("returns ProfileHasKey when app value != profile value", () => {
+          let app = new Entitlement("key", true);
+          let profile = new Entitlement("key", false);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileHasKey;
+          expect(actual).to.equal(expected);
+
+          app = new Entitlement("key", false);
+          profile = new Entitlement("key", true);
+
+          actual = Entitlement.compare(profile, app);
+          expected = EntitlementComparisonResult.ProfileHasKey;
+          expect(actual).to.equal(expected);
+        });
+      });
     });
   });
 });
