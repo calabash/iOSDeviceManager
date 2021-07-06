3f18b1513411d61390958c59ae798eb488baf3dc
AlexeyBudynkov <77280103+AlexeyBudynkov@users.noreply.github.com>
2021-07-06 19:05:37 +0300

    rspec tests passed for iOS 15 after fix in run-loop: device list acquiring

--- iOSDeviceManager/IDMVersionDefines.h
+++ iOSDeviceManager/IDMVersionDefines.h
@@ -1,6 +1,6 @@
 /*
  Do Not Manually Edit This File
 */
-#define IDM_GIT_SHORT_REVISION @"01d3d20"
+#define IDM_GIT_SHORT_REVISION @"679df5a"
 #define IDM_GIT_BRANCH @"v-abudynkov/idm_adaptation"
 #define IDM_GIT_REMOTE_ORIGIN @"git@github.com:calabash/iOSDeviceManager.git"
