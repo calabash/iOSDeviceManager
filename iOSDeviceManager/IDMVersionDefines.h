31d266d1166c07ffa91fdd7f71e85ce98a0d9e61
AlexeyBudynkov <77280103+AlexeyBudynkov@users.noreply.github.com>
2021-07-13 13:31:56 +0300

    returned back IDMVersionDefines file changed AppStub application code signature

--- iOSDeviceManager/IDMVersionDefines.h
+++ iOSDeviceManager/IDMVersionDefines.h
@@ -1,6 +1,11 @@
+
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 /*
  Do Not Manually Edit This File
 */
-#define IDM_GIT_SHORT_REVISION @"679df5a"
-#define IDM_GIT_BRANCH @"v-abudynkov/idm_adaptation"
+
+#define IDM_GIT_SHORT_REVISION @"94c9a5f"
+#define IDM_GIT_BRANCH @"update-cli"
 #define IDM_GIT_REMOTE_ORIGIN @"git@github.com:calabash/iOSDeviceManager.git"
