cdbcc3e5c2c0953537edb6357acfc66d233630b1
Ilya Bausov <ilya.bausov@akvelon.com>
2021-09-22 11:02:17 +0300

    Reverted changes related to testing.

--- bin/test/unit.sh
+++ bin/test/unit.sh
@@ -24,12 +24,11 @@ XCODE_VERSION=`xcrun xcodebuild -version | head -1 | awk '{print $2}' | tr -d '\
 REPORT="reports/${XC_SCHEME}-${XCODE_VERSION}.xml"
 rm -rf "${REPORT}"
 
-#if [ $(gem list -i xcpretty) = "true" ] && [ "${XCPRETTY}" != "0" ]; then
-#  XC_PIPE="xcpretty -c --report junit --output ${REPORT}"
-#else
-#  XC_PIPE='cat'
-#fi
-XC_PIPE='cat'
+if [ $(gem list -i xcpretty) = "true" ] && [ "${XCPRETTY}" != "0" ]; then
+  XC_PIPE="xcpretty -c --report junit --output ${REPORT}"
+else
+  XC_PIPE='cat'
+fi
 
 info "Will pipe xcodebuild to: ${XC_PIPE}"
 
